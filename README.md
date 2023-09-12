c0d588e01f365087a1a6f6f1b5f493ec982855bd
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2023-09-12 16:01:09 +0700

    hampir kelar

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Nama    : Rifdah Nabilah Rahma
 NPM     : 2206082890
 
 Kelas   : PBP B
+
+Link Adaptable : https://riifood.adaptable.app/
+
+Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas secara step-by-step (bukan hanya sekadar mengikuti tutorial).
+jawab:
+
+Buatlah bagan yang berisi request client ke web aplikasi berbasis Django beserta responnya dan jelaskan pada bagan tersebut kaitan antara urls.py, views.py, models.py, dan berkas html.
+jawab:
+
+Jelaskan mengapa kita menggunakan virtual environment? Apakah kita tetap dapat membuat aplikasi web berbasis Django tanpa menggunakan virtual environment?
+jawab:
+
+Jelaskan apakah itu MVC, MVT, MVVM dan perbedaan dari ketiganya.
+jawab:
---
+++ main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# def show_main(request):
+#     context = {
+#         'name': 'Pak Bepe',
+#         'class': 'PBP A'
+#     }
+
+#     return render(request, "main.html", context)
+
+
+def show_main(request):
+    context = {
+        'nama' : 'Rifdah Nabilah',
+        'buah1': 'Melon', 
+        'buah2': 'Strawberry',
+        'buah3': 'Alpukat',
+        'stok1': 20, 
+        'stok2': 15,
+        'stok3': 25,
+        'harga1': 20000, 
+        'harga2': 12000,
+        'harga3': 10000,
+        'keterangan1': 'Buah Melon yang kaya akan nutrisi mengandung kalium, asam folat, protein, vitamin C, dan magnesium.',
+        'keterangan2': 'Buah Strawberry yang kaya akan antioksidan, vitamin C, kalium, vitamin B9(asam folat), dan mangan.',
+        'keterangan3': 'Buah Alpukat mengandung  nutrisi, vitamin B, vitamin K, kalium, tembaga, vitamin E, dan vitamin C.',
+    }
+
+    return render(request, "main.html", context)
