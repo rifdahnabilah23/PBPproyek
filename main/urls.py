b7221f68d7a97f8f45a76dc26d5b6f1594e7ef0d
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2023-11-22 09:55:41 +0700

    Tugas 9 selesai

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 from main.views import register 
-from main.views import login_user
+# from main.views import login_user
 from main.views import logout_user
 
 from main.views import add_stock
@@ -25,7 +25,7 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
+    # path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('add-stock/<int:product_id>/', add_stock, name='add_stock'),
     path('delete-product/<int:product_id>/', delete_product, name='delete_product'),
--- main/views.py
+++ main/views.py
@@ -29,7 +29,7 @@ def show_main(request):
     banyak_item = len(item)
 
     context = {
-        'nama' : request.user.username,
+        'nama' : request.user,
         'class' : 'PBP B',
         'products': products,
         'banyak_item': banyak_item,
@@ -78,20 +78,20 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main")) 
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
+# def login_user(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             response = HttpResponseRedirect(reverse("main:show_main")) 
+#             response.set_cookie('last_login', str(datetime.datetime.now()))
+#             return response
+#         else:
+#             messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+#     context = {}
+#     return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -218,6 +218,7 @@ def create_product_flutter(request):
             user = request.user,
             name = data["name"],
             price = int(data["price"]),
+            amount = int(data["amount"]),
             description = data["description"]
         )
 
